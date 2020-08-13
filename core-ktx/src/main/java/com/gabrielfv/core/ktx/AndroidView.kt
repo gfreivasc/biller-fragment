32138c022e0af32a0e3fcc97660823747addea1b
Gabriel Vasconcelos <gabriel.vasconcelos@picpay.com>
2020-08-13 17:04:27 -0300

    Cleaning up `core-ktx`

--- core-ktx/src/main/java/com/gabrielfv/core/ktx/AndroidView.kt
+++ core-ktx/src/main/java/com/gabrielfv/core/ktx/AndroidView.kt
@@ -3,4 +3,4 @@ package com.gabrielfv.core.ktx
 import android.view.LayoutInflater
 import android.view.View
 
-val View.inflater get() = LayoutInflater.from(context)
+val View.inflater: LayoutInflater get() = LayoutInflater.from(context)
--- core-ktx/src/main/java/com/gabrielfv/core/ktx/ViewBinding.kt
+++
@@ -1,2 +0,0 @@
-package com.gabrielfv.core.ktx
-
